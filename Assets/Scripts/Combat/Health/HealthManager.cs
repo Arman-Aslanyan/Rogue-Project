5406076580af206b8dc4a88f00569c27290157ec
David-Avetian <davavetian@gmail.com>
2021-12-20 10:45:58 -0800

    Health Scrip barebones done
    My numbers

--- Assets/Scripts/Combat/Health/HealthManager.cs
+++ Assets/Scripts/Combat/Health/HealthManager.cs
@@ -2,28 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HealthManager : MonoBehaviour
 {
-    public int startHp;
-    public int curHp;
+    public string AtomicInteger = "1";
+    public int maxHp = 100;
+    public int curHp = 100;
+    public Text startHpText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startHpText.text = "Health: " + curHp.ToString() + "/ " + maxHp.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public int ChangeHp(int change)
+    public void ChangeHp()
     {
 
-
-        return curHp;
+        if (curHp > 0)
+        {
+            startHpText.text = "Health: " + curHp.ToString() + "/ " + maxHp.ToString();
+        }
+        else
+        {
+            //send to game over scene
+            SceneManager.LoadScene("Game Over");
+        }
     }
 }
