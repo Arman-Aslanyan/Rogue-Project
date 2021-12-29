c7a6628cff22ffba340f11bb062b7403cb262d66
PeachKing0 <arman.aslanyan@digipen.edu>
2021-12-28 23:42:00 -0800

    mini additions

--- Assets/Scripts/Characters/BeginTreantFight.cs
+++ Assets/Scripts/Characters/BeginTreantFight.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class BeginTreantFight : MonoBehaviour
 {
+    public GameObject fakeTreant;
+    private GameObject realTreant;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        realTreant = gameObject;
+        realTreant.SetActive(false);
     }
 
-    public void BeginTheChaos()
+    public void BeginTheChaos(bool canStart)
     {
-
+        if (canStart)
+        {
+            fakeTreant.SetActive(false);
+            realTreant.SetActive(true);
+        }
     }
 }
