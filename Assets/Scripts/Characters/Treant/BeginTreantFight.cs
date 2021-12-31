88821cb8636cecc993d5d7243a1aacfab4033ec0
PeachKing0 <arman.aslanyan@digipen.edu>
2021-12-31 13:22:38 -0800

    Adding onto TreantStartup

--- Assets/Scripts/Characters/Treant/BeginTreantFight.cs
+++ Assets/Scripts/Characters/Treant/BeginTreantFight.cs
@@ -7,6 +7,7 @@ public class BeginTreantFight : MonoBehaviour
     private GameObject fakeTreant;
     public GameObject realTreant;
     private Animator anim;
+    private Animator orngAnim;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,8 @@ public class BeginTreantFight : MonoBehaviour
         anim = GetComponent<Animator>();
         anim.enabled = false;
         realTreant.SetActive(false);
+        orngAnim = GameObject.FindGameObjectWithTag("Sir Orange").GetComponent<Animator>();
+        orngAnim.enabled = false;
     }
 
     public void BeginTheChaos(bool canStart)
@@ -37,7 +40,36 @@ public class BeginTreantFight : MonoBehaviour
         npc.boxSprite.enabled = false;
         npc.speechBox.text = "";
         npc.speechBox.enabled = false;
+        Transform orange = GameObject.FindGameObjectWithTag("Sir Orange").transform;
+        orngAnim.enabled = true;
+        yield return new WaitForSeconds(3);
+        orngAnim.enabled = false;
+        npc.dialogue[1] = "OH HO HO! Well thats your problem seeya he he he HA!!!!";
+        StartCoroutine(npcSpeak(npc.dialogue[1], npc));
+        yield return new WaitForSeconds(5);
+        orange.position = new Vector3(-1.5f, 5.3f, 0);
+        Destroy(orange);
         fakeTreant.SetActive(false);
         realTreant.SetActive(true);
+    }
+
+    public IEnumerator npcSpeak(string str, NPC npc)
+    {
+        npc.boxSprite.enabled = true;
+        //Auto-Type
+        foreach (char character in str.ToCharArray())
+        {
+            npc.speechBox.text += character;
+            yield return new WaitForSeconds(npc.typingSpeed);
+        }
+        StartCoroutine(IBetBlakeWillTestThis(npc));
+    }
+
+    public IEnumerator IBetBlakeWillTestThis(NPC npc)
+    {
+        yield return new WaitForSeconds(1);
+        npc.boxSprite.enabled = false;
+        npc.speechBox.text = "";
+        npc.speechBox.enabled = false;
     }
 }
