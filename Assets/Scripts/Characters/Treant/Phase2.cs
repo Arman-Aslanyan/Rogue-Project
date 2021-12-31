c1a2de28eb35b3e31eb816d964a79bca276aea82
David-Avetian <67847173+Molten57@users.noreply.github.com>
2021-12-30 21:19:59 -0800

    Fixed animation and fixed the acorn prefab bug

--- Assets/Scripts/Characters/Treant/Phase2.cs
+++ Assets/Scripts/Characters/Treant/Phase2.cs
@@ -5,20 +5,47 @@ using UnityEngine;
 public class Phase2 : MonoBehaviour
 {
     public Animator animator;
+    bool isTransformGoneOff = false;
+    public Rigidbody2D rb;
+    public GameObject acornBig;
+    Shooting shoot;
+
+
+    private void Start()
+    {
+        shoot = GetComponent<Shooting>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         HealthManager hpMan = GetComponent<HealthManager>();
 
+
         if (hpMan.curHp <= 50)
         {
             //Treant go BBRRRRRR
-            Shooting shut = GetComponent<Shooting>();
-            shut.projectile.transform.localScale = new Vector3(2, 2, 1);
-            shut.baseDmg = 10;
-            shut.fireRate = 2;
-            shut.travelRate = 14;
-            animator.SetBool("isTransforming", true);
+            shoot.projectile = acornBig;
+            shoot.baseDmg = 10;
+            shoot.fireRate = 2;
+            shoot.travelRate = 14;
+            if (!isTransformGoneOff)
+            {
+                animator.SetBool("isTransforming", true);
+                rb.constraints = RigidbodyConstraints2D.FreezeAll;
+                shoot.enabled = false;
+                StartCoroutine(wait());
+            }
         }
     }
+
+    public IEnumerator wait()
+    {
+        yield return new WaitForSeconds(0.6f);
+        animator.SetBool("isTransforming", false);
+        shoot.enabled = true;
+        isTransformGoneOff = true;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
 }
